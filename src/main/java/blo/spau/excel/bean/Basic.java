b7685bb66c8df53c705324e05e0d3da132670dec
BLODUC SPAUTER <3230695439@qq.com>
2024-03-02 00:34:26 +0800

    Initial push

---
+++ src/main/java/blo/spau/excel/bean/Basic.java
@@ -0,0 +1,13 @@
+package blo.spau.excel.bean;
+
+public class Basic {
+    private  String dateformat="yyyy-MM-dd";
+
+    public String getDateformat() {
+        return dateformat;
+    }
+
+    public void setDateformat(String dateformat) {
+        this.dateformat = dateformat;
+    }
+}
